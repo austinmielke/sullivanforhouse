936ae5daa69bdcd014f6dee42160551b3c930bbb
Austin Mielke <amielkedev@gmail.com>
2018-04-02 19:01:14 -0500

    Added meta data for sharing on social media sites

--- resources/views/includes/head.blade.php
+++ resources/views/includes/head.blade.php
@@ -2,9 +2,17 @@
 <meta name="viewport" content="width=device-width, initial-scale=1.0">
 <meta http-equiv="X-UA-Compatible" content="ie=edge">
 
+<title>Kelly Sullivan for House 2018 @yield('title')</title>
+
 <meta name=”description” content="Kelly is running for the SD House of Representatives, because she believes the people of District 13 need a strong voice that is willing to stand up and engage.">
 
-<title>Kelly Sullivan for House 2018 @yield('title')</title>
+<meta property="og:image" content="{{ URL::asset('img/social-media.jpg') }}">
+<meta property="og:type" content="website">
+<meta property="og:url" content="https://sullivanforhouse.com">
+<meta property="og:title" content="Kelly Sullivan for House 2018">
+<meta property="og:description" content="Together, we will affect positive change for South Dakota.">
+
+
 
 <link rel="stylesheet" href="{{ URL::asset('css/main.css') }}">
 
