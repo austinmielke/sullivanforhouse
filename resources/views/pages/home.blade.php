eb1c78dc15363afb563b5f463ef53ac4ba4fe456
Austin Mielke <amielkedev@gmail.com>
2018-08-01 18:05:03 -0500

    Reworked front page with new copy and SVG's

--- resources/views/pages/home.blade.php
+++ resources/views/pages/home.blade.php
@@ -13,17 +13,20 @@
     <div class="py-4">
         <h2 class="text-center text-4xl mb-4">My Vision</h2>
         <div class="container mx-auto px-2 md:flex">
+                <div class="md:w-1/3">
+                    <img class="m-auto block h-32 lg:h-48 p-2" src="{{ URL:: asset('img/equal.svg') }}" alt="an equal sign">
+                    <h3 class="text-center text-2xl p-2">Equality</h3>
+                    <p class="text-xl p-2 leading-normal">All South Dakotans deserve to have equal rights and dignity. I will stand strong and be the voice in Pierre, fighting for LGBTQ equality and I will stand up for immigrants, Muslims, refugees, Native Americans, women, people of color, people with disabilities, our elderly and aging population, and beyond. I believe that South Dakota must be represented equally and be inclusive of those whose voices have been silenced. I will stand up for you, South Dakota.</p>
+                </div>
             <div class="md:w-1/3">
-                <img class="m-auto block h-32 lg:h-48" src="{{ URL:: asset('img/handshake.svg') }}" alt="handshake">
-                <p class="text-xl p-2 leading-normal">South Dakota deserves a state government that is willing to work together and collaborate on policy that represents all South Dakotans, not the personal or professional interests of politicians.</p>
+                <img class="m-auto block h-32 lg:h-48 p-2" src="{{ URL:: asset('img/open-book.svg') }}" alt="an open book">
+                <h3 class="text-center text-2xl p-2">Education</h3>
+                <p class="text-xl p-2 leading-normal">I believe that investing in education is vital for the future of South Dakota. We must ensure that high-quality education, beginning in pre-kindergarten, is a priority, we must pay teachers competitive wages to attract and retain talent, we must make higher education affordable and accessible to ALL, and we must expand technical education to ensure a skilled and ready workforce.</p>
             </div>
             <div class="md:w-1/3">
-                <img class="m-auto block h-32 lg:h-48" src="{{ URL:: asset('img/megaphone.svg') }}" alt="megaphone">
-                <p class="text-xl p-2 leading-normal">We must elect leaders who stand up for freedom and liberty and allow all South Dakotans to pursue a life of happiness.</p>
-            </div>
-            <div class="md:w-1/3">
-                <img class="m-auto block h-32 lg:h-48" src="{{ URL:: asset('img/people.svg') }}" alt="people">
-                <p class="text-xl p-2 leading-normal">We must build a future for our children by enacting social and economic policies that are forward-minded and will not only keep South Dakotans here, but will bring the best and brightest to our great state.</p>
+                <img class="m-auto block h-32 lg:h-48 p-2" src="{{ URL:: asset('img/piggy-bank.svg') }}" alt="a piggy bank">
+                <h3 class="text-center text-2xl p-2">Economic Opportunity</h3>
+                <p class="text-xl p-2 leading-normal">As South Dakota continues to grow, we must find ways to allow ALL South Dakotans to thrive. We must shift our government to be open and transparent, ensuring your hard-earned tax-dollars are spent wisely, with all South Dakotans as the number one priority. We must pay a living wage so South Dakotans can support themselves and their families. We must attract innovative companies to South Dakota to ensure stable jobs and continuing economic growth.</p>
             </div>
         </div>
     </div>
